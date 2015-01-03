8226f1a2f5b8a7ef0944496dc1ae9e48fb2063ec
Satyagraha <satyagraha.1956@googlemail.com>
2015-01-03 21:26:10 +0000

    Updated CSS; require JVM 1.7

--- plugin.tests/source/code/satyagraha/gfm/support/TransformerTest.java
+++ plugin.tests/source/code/satyagraha/gfm/support/TransformerTest.java
@@ -15,7 +15,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.runners.MockitoJUnitRunner;
 
